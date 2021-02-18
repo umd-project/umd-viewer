950927eada504483627beb4a5ecde54dcabd11e7
RmR <connect2rmr@gmail.com>
2021-02-18 12:37:49 +0530

    added umd-components

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import json from '@rollup/plugin-json';
 export default {
   input: './src/umd-viewer-src.min.js',
   output: {
-    file: './dist/umd-viewer-min.js',
+    file: './dist/umd-viewer.min.js',
     format: 'es'
   },
   plugins: [nodeResolve(), commonjs(), json()]
